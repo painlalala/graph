c5d18a50f4bd915539a38d2c5c2b3d7667a28aef
Lozynskyi Nazarii <nazarii.lozynskyi@lantec.ua>
2023-10-13 18:08:11 +0300

    ✨fix

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const pca = new msal.ConfidentialClientApplication(msalConfig)
 
 const authCodeUrlParameters = {
   scopes: ['User.ReadWrite.All', 'User.Read.All'],
-  redirectUri: 'http://localhost:3000/auth/callback',
+  redirectUri: 'http://0.0.0.0:3000/auth/callback',
 };
 
 app.use(session({
